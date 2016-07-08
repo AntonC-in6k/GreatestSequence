1313e13eef071d1415fe191405cd8763b247bc17
employee <antonc@softwareplanet.uk.com>
2016-07-08 15:20:45 +0300

    test for one element in sequence

--- src/GreatestSequence.java
+++ src/GreatestSequence.java
@@ -7,6 +7,7 @@ public class GreatestSequence {
 
     public static ArrayList<Integer> findSequence(ArrayList<Integer> input) {
         if (input==null||input.size()==0){return new ArrayList<Integer>();}
+        if (input.size()==1){return input;}
     return null;
     }
 }
--- test/GreatestSequenceTest.java
+++ test/GreatestSequenceTest.java
@@ -4,6 +4,7 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
@@ -11,12 +12,19 @@ import static org.junit.Assert.assertThat;
 public class GreatestSequenceTest {
 
     @Test
-    public void nullTest() throws Exception {
+    public void nullSequenceTest() throws Exception {
         assertThat(GreatestSequence.findSequence(null), is(new ArrayList<Integer>()));
     }
 
     @Test
-    public void emptyLitTest() throws Exception {
+    public void emptySequenceTest() throws Exception {
         assertThat(GreatestSequence.findSequence(new ArrayList<Integer>()), is(new ArrayList<Integer>()));
+    }
+
+    @Test
+    public void oneDigitInSequenceTest() throws Exception{
+        ArrayList<Integer> testSequence = new ArrayList<Integer>(Arrays.asList(new Integer[]{1}));
+        ArrayList<Integer> expectedSequence = new ArrayList<Integer>(Arrays.asList(new Integer[]{1}));
+        assertThat(GreatestSequence.findSequence(testSequence),is(expectedSequence));
     }
 }
