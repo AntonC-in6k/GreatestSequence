457f809926660d91340893bbac631b068da493db
employee <antonc@softwareplanet.uk.com>
2016-07-08 15:02:34 +0300

    test for null

---
+++ src/GreatestSequence.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by employee on 7/8/16.
+ */
+public class GreatestSequence {
+
+    public static ArrayList<Integer> findSequence(ArrayList<Integer> input) {
+        if (input==null){return new ArrayList<Integer>();}
+    return null;
+    }
+}
---
+++ test/GreatestSequenceTest.java
@@ -0,0 +1,19 @@
+/**
+ * Created by employee on 7/8/16.
+ */
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class GreatestSequenceTest {
+
+    @Test
+    public void nullTest() throws Exception {
+        assertThat(GreatestSequence.findSequence(null), is(new ArrayList<Integer>()));
+    }
+
+
+}
